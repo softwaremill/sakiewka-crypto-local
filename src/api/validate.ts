cb1cefe38aa2f4e50f4da80a79b15c90ff348501
ledwonmat <mateusz.ledwon@softwaremill.com>
2018-07-31 16:10:43 +0200

    uses proper Joi types

--- src/api/validate.ts
+++ src/api/validate.ts
@@ -1,10 +1,10 @@
 import { Request } from 'express'
-import { ObjectSchema, ValidationError } from 'joi'
+import { ObjectSchema, ValidationErrorItem } from 'joi'
 
 export default (request: Request, model: ObjectSchema, authRequired?: boolean) => {
   const { error } = model.validate(request.body)
 
-  if (error) return error.details.map((err: ValidationError) => err.message)
+  if (error) return error.details.map((err: ValidationErrorItem) => err.message)
 
   if (authRequired && !request.rawHeaders.includes('Authorization')) {
     return ['Request header Authorization is required.']
