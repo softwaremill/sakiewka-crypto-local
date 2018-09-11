da5a87da1f59d2f51ffe63f7d0247db41b3284be
ledwonmat <mateusz.ledwon@softwaremill.com>
2018-09-11 11:21:07 +0200

    remove console log and use single quotes

--- src/api/tests/integration/app.test.ts
+++ src/api/tests/integration/app.test.ts
@@ -383,16 +383,15 @@ describe('server', () => {
       const response = await supertest(app)
         .post(`/${constants.BASE_API_PATH}/eth/wallet/sign`)
         .send({
-          pk: "82d052c865f5763aad42add438569276c00d3d88a2d062d36b2bae914d58b8c8",
-          address: "0x2191ef87e392377ec08e7c08eb105ef5448eced5",
-          amount: "200",
-          data: "",
+          pk: '82d052c865f5763aad42add438569276c00d3d88a2d062d36b2bae914d58b8c8',
+          address: '0x2191ef87e392377ec08e7c08eb105ef5448eced5',
+          amount: '200',
+          data: '',
           expireTime: new Date().getTime() + 3600000 /* one hour from now */,
           contractNonce: 1
         })
 
       expect(response.status).to.be.equal(200)
-      console.log(`This is it: ${response.body.data}`)
     })
   })
 
@@ -401,16 +400,15 @@ describe('server', () => {
       const response = await supertest(app)
         .post(`/${constants.BASE_API_PATH}/eth/wallet/tokenSign`)
         .send({
-          pk: "82d052c865f5763aad42add438569276c00d3d88a2d062d36b2bae914d58b8c8",
-          address: "0x2191ef87e392377ec08e7c08eb105ef5448eced5",
-          amount: "200",
-          contractAddress: "0x2932b7a2355d6fecc4b5c0b6bd44cc31df247a2e",
+          pk: '82d052c865f5763aad42add438569276c00d3d88a2d062d36b2bae914d58b8c8',
+          address: '0x2191ef87e392377ec08e7c08eb105ef5448eced5',
+          amount: '200',
+          contractAddress: '0x2932b7a2355d6fecc4b5c0b6bd44cc31df247a2e',
           expireTime: new Date().getTime() + 3600000 /* one hour from now */,
           contractNonce: 1
         })
 
       expect(response.status).to.be.equal(200)
-      console.log(`This is it: ${response.body.data}`)
     })
   })
 })
