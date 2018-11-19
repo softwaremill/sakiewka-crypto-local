98a8c5f74d0a2dbb39edf49eb1186d5bcf638d97
ghostbuster91 <kghost0@gmail.com>
2018-11-19 19:35:20 +0100

    Change env variables because of names conflict with other tools

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,7 +1,7 @@
 import app from './app'
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const host = process.env.HOST || '0.0.0.0'
+const port = parseInt(process.env.APP_SELF_PORT, 10) || 3000
+const host = process.env.APP_SELF_HOST || '0.0.0.0'
 
 const server = app.listen(port, host, () => {
   console.log(`Sakiewka-client server is running at http://${host}:${port}`)
