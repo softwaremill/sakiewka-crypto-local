9a6563ab6dbb12d7a7a779d9192cc3b8eeccd183
ghostbuster91 <kghost0@gmail.com>
2019-04-30 17:11:40 +0200

    Write yaml to file

--- api/api/src/main/scala/sakiewka/local/Main.scala
+++ api/api/src/main/scala/sakiewka/local/Main.scala
@@ -4,13 +4,18 @@ import sakiewka.local.chain.ChainApi
 import sakiewka.local.core.{TransferApi, UserApi}
 import tapir.docs.openapi._
 import tapir.openapi.circe.yaml._
+import java.nio.file.{Paths, Files}
+import java.nio.charset.StandardCharsets
 
-object Api extends App {
+object Main extends App {
+
+  require(args.length == 1, "Required path, where to generate yaml file")
 
   val endpoints = UserApi.endpoints ++
     new TransferApi().endpoints ++
     new ChainApi("btc").endpoints ++
     new ChainApi("btg").endpoints
 
-  println(endpoints.toOpenAPI("sakiewka-crypto-local", "1.0").toYaml)
+  val yaml = endpoints.toOpenAPI("sakiewka-crypto-local", "1.0").toYaml
+  Files.write(Paths.get(args(0)), yaml.getBytes(StandardCharsets.UTF_8))
 }
