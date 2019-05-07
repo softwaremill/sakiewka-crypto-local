0694c394bbb1d66d339d627eb781833125ba2cfe
ghostbuster91 <kghost0@gmail.com>
2019-05-07 16:29:52 +0200

    Add fee-rate endpoint

---
+++ src/api/tests/chain/bitcoin/get-fee-rate.test.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+import app from '../../../app'
+import supertest from 'supertest'
+import { currency } from '../../helpers'
+import { constants } from 'sakiewka-crypto'
+// @ts-ignore
+const { feeRates } = app.sakiewkaApi[currency]
+
+// @ts-ignore
+const mockFn = jest.fn(() => {
+  return new Promise((resolve: Function) => {
+    resolve('test fee')
+  })
+})
+
+feeRates.getFeeRate = mockFn
+
+describe(`/${currency}/fee-rate`, () => {
+  it('should return fee rate', async () => {
+    const response = await supertest(app)
+      .get(`/${constants.BASE_API_PATH}/${currency}/fee-rate`)
+
+    expect(response.status).to.be.equal(200)
+    const data = response.body.data
+    expect(data).to.eq('test fee')
+  })
+})
